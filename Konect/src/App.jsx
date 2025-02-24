d09182f1b655b34b2093122203652e515021cfb4
imsarann <shitisaesthetic@gmail.com>
2025-02-24 23:04:24 +0530

    added react components with dashboard page

--- Konect/src/App.jsx
+++ Konect/src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './components/Login';
 import Register from './components/Register';
 import './App.css'
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
+            </Routes>
+            <Navbar/>
+            <Sidebar/>
+            <Routes>
                 <Route path="/register" element={<Register />} />
                 <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
---
+++ Konect/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Post from '../components/Post'
+
+export default function Dashboard() {
+  return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      {/* <Post post={"asdf"} /> */}
+    </div>
+  )
+}
