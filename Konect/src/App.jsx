10ad978b736cfc2fad132a6e03f763185b526ed7
Rishi250605 <221501114@rajalakshmi.edu.in>
2025-02-24 23:10:31 +0530

    altered

--- Konect/src/App.jsx
+++ Konect/src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/" element={<Navigate to="/login" replace />} />
-            </Routes>
+            <div className="app-container">
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/" element={<Navigate to="/login" replace />} />
+                </Routes>
+                <Footer />
+            </div>
         </Router>
     );
 }
